dce4774c4a7fe07ab1dfbf4c803228721783e5c5
thiagotp <thiagoteixeira.ps@gmail.com>
2021-03-18 15:07:51 -0300

    ajustando o controller e a função para carregar os arquivos que quero enviar

---
+++ app/public/src/controller/dropboxController.js
@@ -0,0 +1,63 @@
+class DropBoxController {
+
+    constructor() {
+
+        this.btnSendFile = document.querySelector('#btn-send-file')
+        this.inputFiles = document.querySelector('#files')
+        this.progressBar = document.querySelector('#react-snackbar-root')
+        this.initEvents();
+
+    }
+
+    initEvents() {
+
+        this.btnSendFile.addEventListener('click', (e) => {
+
+            this.inputFiles.click()
+
+        });
+
+        this.inputFiles.addEventListener('change', (e) => {
+
+            console.log(e.target.files);
+            this.uploadTask(e.target.files)
+            this.progressBar.style.display = 'block';
+
+        })
+    }
+
+    uploadTask(files) {
+
+        let promises = [];
+
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST', '/upload');
+                ajax.onload = event => {
+
+                    try {
+                        resolve.JSON.parse(ajax.responseText)
+                    } catch (e) {
+                        reject(e)
+                    }
+
+                }
+
+                ajax.onerror = event => {
+                    reject(event);
+                }
+
+                let formData = new FormData();
+
+                formData.append('input-file', file);
+                ajax.send(formData);
+
+            }))
+        })
+
+        return Promise.all(promises)
+
+    }
+}
